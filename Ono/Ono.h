969bd856195cba4f5d22b359a10741078b10014c
jayhickey <johnrhickey@gmail.com>
2016-01-02 17:31:15 -0500

    Added watchOS 2 target
    More updates for watchOS 2
    
    Made watch scheme shared
    
    Updates
    
    Update

---
+++ Ono/Ono.h
@@ -0,0 +1,19 @@
+//
+//  Ono.h
+//  Ono
+//
+//  Created by johnrhickey on 1/2/16.
+//  Copyright © 2016 Mattt Thompson. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+
+//! Project version number for Ono.
+FOUNDATION_EXPORT double OnoVersionNumber;
+
+//! Project version string for Ono.
+FOUNDATION_EXPORT const unsigned char OnoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Ono/PublicHeader.h>
+
+#import <Ono/ONOXMLDocument.h>
